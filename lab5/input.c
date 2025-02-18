7de450cb643076962720806d45e678fbf594f4a1
tejasvinigoel <128983594+tejasvinigoel@users.noreply.github.com>
2025-02-18 11:04:52 +0530

    Add files via upload

---
+++ lab5/input.c
@@ -0,0 +1,13 @@
+//this program just adds two numbers;
+#include <stdio.h>
+int main() {
+int a, b, c;
+/* This is a
+multi-line comment */
+a = b+ c; //addition of b and c is stored in c
+printf("%d", a); //print the value of a;
+/* done with
+the code
+and just stop it */
+return 0;
+}
---
+++ lab5/output.c
@@ -0,0 +1,10 @@
+
+#include <stdio.h>
+int main() {
+int a, b, c;
+
+a = b+ c; 
+printf("%d", a); 
+
+return 0;
+}
